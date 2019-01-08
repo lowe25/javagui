cc55b899054516ef387c3ad2988c936ab11a3348
lowe25 <louiealdrin25@gmail.com>
2019-01-08 13:18:04 +0800

    lelz

---
+++ BasicElements.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class BasicElements 
+{
+		public static void main(String[]args)
+		{
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+			frame.setSize(900, 600);
+		System.out.println("HELLO WORLD");
+		}
+
+}
--- class BasicElements.java
+++
@@ -1,7 +0,0 @@
-class BasicElements
-{
-	public static void main(String[]args)
-	{
-	System.out.println("HELLO WORLD");
-	}
-}
