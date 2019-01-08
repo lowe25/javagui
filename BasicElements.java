9c88cef2e63c2e14be35ad18d4b52e2e65a31125
lowe25 <louiealdrin25@gmail.com>
2019-01-08 13:34:54 +0800

    JFRAME WITH BUTTON

--- BasicElements.java
+++ BasicElements.java
@@ -1,9 +1,19 @@
+import javax.swing.*;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+
 public class BasicElements 
 {
 		public static void main(String[]args)
 		{
-			JFrame frame = new JFrame();
+			JFrame frame = new JFrame("SIMPLE GUI");
+			JButton button = new JButton("BUTTON");
+              button.setSize(200,50);
+
+              frame.setLayout(null);
+              
+              button.setVisible(true);
+		     frame.add(button);
 			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 			frame.setSize(900, 600);
